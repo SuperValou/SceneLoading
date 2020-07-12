40e0e2f7645ec2f9eb01b0d645933baaa66181b6
SuperValou <valentin.esmieu@tuta.io>
2020-07-12 18:39:34 +0200

    Clean using statements

--- Assets/Scripts/LoadingSystems/SceneLoadings/SceneLoadingSystem.cs
+++ Assets/Scripts/LoadingSystems/SceneLoadings/SceneLoadingSystem.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Threading;
 using Assets.Scripts.LoadingSystems.SceneLoadings.SceneInfos;
 using UnityEngine;
 using UnityEngine.SceneManagement;
