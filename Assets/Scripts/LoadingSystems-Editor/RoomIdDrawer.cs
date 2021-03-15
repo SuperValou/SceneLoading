9081c2a44c769bc602e60e7ce1ebaf9111889f34
SuperValou <valentin.esmieu@tuta.io>
2021-03-15 11:12:58 +0100

    Simplify and improve a bit RoomIdDrawer

--- Assets/Scripts/LoadingSystems-Editor/RoomIdDrawer.cs
+++ Assets/Scripts/LoadingSystems-Editor/RoomIdDrawer.cs
@@ -10,30 +10,37 @@ namespace Assets.Scripts
     [CustomPropertyDrawer(typeof(RoomIdAttribute))]
     public class RoomIdDrawer : PropertyDrawer
     {
-        private List<Tuple<int, string>> _options = new List<Tuple<int, string>>();
+        private static bool _initialized;
+        private static List<int> _optionIds;
+        private static GUIContent[] _displayedOptions;
 
         private int _selectedIndex;
 
         public RoomIdDrawer() : base()
         {
-            var roomInfos = SceneInfo.GetAll().Where(si => SceneInfo.IsRoom(si.Id)).ToArray();
-
-            foreach (var roomInfo in roomInfos)
+            if (_initialized)
             {
-                int id = (int)roomInfo.Id;
-                string displayName = $"{roomInfo.SceneName} [{(int) roomInfo.Id}: {roomInfo.Id}]";
-                var tuple = new Tuple<int, string>(id, displayName);
-                _options.Add(tuple);
+                return;
             }
+
+            var roomInfos = SceneInfo.GetAll().Where(si => SceneInfo.IsRoom(si.Id)).ToList();
+            _optionIds = roomInfos.Select(ri => (int) ri.Id).ToList();
+            _displayedOptions = roomInfos.Select(roomInfo =>
+            {
+                string displayedName = $"{roomInfo.SceneName} [{roomInfo.Id}={(int) roomInfo.Id}]";
+                return new GUIContent(displayedName);
+            }).ToArray();
+            
+            _initialized = true;
         }
 
         // Here you can define the GUI for your property drawer. Called by Unity.
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            int index = _options.FindIndex(tuple => tuple.Item1 == property.intValue);
-            EditorGUI.LabelField(position, property.name);
-            int selectedIndex = EditorGUI.Popup(position, index, _options.Select(o => o.Item2).ToArray());
-            property.intValue = _options[selectedIndex].Item1;
+            int previousSelectedIndex = _optionIds.FindIndex(id => id == property.intValue);
+            var guiContent = new GUIContent(property.displayName);
+            int newSelectedIndex = EditorGUI.Popup(position, guiContent, previousSelectedIndex, _displayedOptions.Select(d => new GUIContent(d)).ToArray() );
+            property.intValue = _optionIds[newSelectedIndex];
         }
     }
 }
