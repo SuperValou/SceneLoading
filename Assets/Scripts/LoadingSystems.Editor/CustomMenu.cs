4ac509d81735696b0a989a412d0d933967736aa4
SuperValou <valentin.esmieu@tuta.io>
2021-03-16 10:32:02 +0100

    Draft calling code

---
+++ Assets/Scripts/LoadingSystems.Editor/CustomMenu.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.LoadingSystems.SceneInfos;
+using UnityEditor;
+
+namespace Assets.Scripts.LoadingSystems.Editor
+{
+    public static class CustomMenu
+    {
+        [MenuItem("Rooms/Create new Room")]
+        public static void ShowCreateNewRoomWindow()
+        {
+            //EditorWindow.GetWindow<ScriptLinkWindow>();
+        }
+
+        [MenuItem("Rooms/Generate " + nameof(SceneId) + " enumeration")]
+        public static void ShowGenerateEnumerationWindow()
+        {
+            var toto = new SceneInfoGenerator();
+            toto.DoStuff();
+        }
+    }
+}
---
+++ Assets/Scripts/LoadingSystems.Editor/SceneInfoGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using Assets.Scripts.LoadingSystems.SceneInfos;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.LoadingSystems.Editor
+{
+    public class SceneInfoGenerator
+    {
+        private readonly Regex _enumMemberRegex = new Regex(@"[^\w]");
+
+        public void DoStuff()
+        {
+            var scenesGuids = AssetDatabase.FindAssets("t:Scene");
+            var scenesPaths = scenesGuids.Select(AssetDatabase.GUIDToAssetPath);
+            var sceneNames = scenesPaths.Select(Path.GetFileNameWithoutExtension).ToList();
+
+            var distinctNames = sceneNames.Select(name => name.ToLowerInvariant()).Distinct();
+            if (distinctNames.Count() != sceneNames.Count)
+            {
+                throw new InvalidOperationException("Two scenes share the same case-insensitive name.");
+            }
+
+            string templateFilePath = GetTemplatePath();
+
+            string text = File.ReadAllText(templateFilePath);
+
+            foreach (var sceneName in sceneNames)
+            {
+                
+            }
+        }
+
+        private string GetTemplatePath([CallerFilePath] string currentScriptFilePath = "")
+        {
+            string currentDirectory = Directory.GetParent(currentScriptFilePath).FullName;
+            string templateFilePath = Path.Combine(currentDirectory, "SceneIdTemplate.txt");
+            return templateFilePath;
+        }
+    }
+}
