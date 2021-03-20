feef14975fc85f5d9c22ac19aa7a8f5348e84c7f
SuperValou <valentin.esmieu@tuta.io>
2021-03-20 18:18:57 +0100

    Extract AssetDatabase utility methods into static class

--- Assets/Scripts/LoadingSystems.Editor/SceneInfoGenerations/SceneInfoGenerator.cs
+++ Assets/Scripts/LoadingSystems.Editor/SceneInfoGenerations/SceneInfoGenerator.cs
@@ -17,9 +17,8 @@ namespace Assets.Scripts.LoadingSystems.Editor.SceneInfoGenerations
         public void Generate()
         {
             // Get all scenes
-            var scenesGuids = AssetDatabase.FindAssets("t:Scene");
-            var scenesPaths = scenesGuids.Select(AssetDatabase.GUIDToAssetPath);
-            var sceneNames = scenesPaths.Select(Path.GetFileNameWithoutExtension).ToList();
+            var scenePaths = AssetDatabaseExt.GetAllScenePaths();
+            var sceneNames = scenePaths.Select(Path.GetFileNameWithoutExtension).ToList();
 
             var distinctNames = sceneNames.Select(name => name.ToLowerInvariant()).Distinct();
             if (distinctNames.Count() != sceneNames.Count)
@@ -28,11 +27,11 @@ namespace Assets.Scripts.LoadingSystems.Editor.SceneInfoGenerations
             }
 
             // Find SceneId file path
-            string destinationFilePath = GetAssetPath(nameof(SceneId), ".cs");
-            Debug.Log("Rewriting to " + destinationFilePath);
+            string destinationFilePath = AssetDatabaseExt.GetAssetFilePath($"{nameof(SceneId)}.cs");
+            Debug.Log($"About to rewrite file at '{destinationFilePath}'...");
 
             // Get template
-            string templatePath = GetAssetPath("SceneIdTemplate", ".txt");
+            string templatePath = AssetDatabaseExt.GetAssetFilePath("SceneIdTemplate.txt");
             var parser = new TemplateParser(templatePath);
 
             parser.Parse();
@@ -82,38 +81,6 @@ namespace Assets.Scripts.LoadingSystems.Editor.SceneInfoGenerations
             // Write template to file
             SessionWriter writer = new SessionWriter();
             writer.WriteSession(session, destinationFilePath);
-        }
-
-        private string GetAssetPath(string assetName, string assetExtension)
-        {
-            if (string.IsNullOrEmpty(assetName))
-            {
-                throw new ArgumentException("Value cannot be null or empty.", nameof(assetName));
-            }
-
-            var paths = AssetDatabase.FindAssets(assetName).Select(AssetDatabase.GUIDToAssetPath).ToArray();
-            string relativePath = null;
-            foreach (var path in paths)
-            {
-                string filename = Path.GetFileNameWithoutExtension(path);
-                string extension = Path.GetExtension(path);
-
-                if (filename == assetName && extension == assetExtension)
-                {
-                    relativePath = path; // looks like "Assets/Scripts/.../<script name>.cs"
-                    break;
-                }
-            }
-
-            if (relativePath == null)
-            {
-                throw new InvalidOperationException($"Unable to find '{assetName}' asset. Results were: '{string.Join(", ", paths)}' ({paths.Length} results).");
-            }
-
-            string root = Application.dataPath; // returns "<path to project folder>/Assets"
-            relativePath = relativePath.Substring("Assets/".Length);
-            string fullPath = Path.Combine(root, relativePath);
-            return fullPath;
         }
     }
 }
