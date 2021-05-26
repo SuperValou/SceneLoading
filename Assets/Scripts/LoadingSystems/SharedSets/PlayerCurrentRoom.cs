28cb6f59361d464c3373554befd1fd20ae1ec527
SuperValou <valentin.esmieu@tuta.io>
2021-05-27 00:11:36 +0200

    Use shared ScriptableObject on DoorManager

---
+++ Assets/Scripts/LoadingSystems/SharedSets/PlayerCurrentRoom.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.LoadingSystems.SceneInfos;
+using UnityEngine;
+
+namespace Assets.Scripts.LoadingSystems.SharedSets
+{
+    [CreateAssetMenu(fileName = nameof(PlayerCurrentRoom), menuName = nameof(LoadingSystems) + "/" + nameof(PlayerCurrentRoom))]
+    public class PlayerCurrentRoom : ScriptableObject
+    {
+        [SerializeField]
+        private SceneId roomId = (SceneId)~0;
+
+        public SceneId RoomId
+        {
+            get => roomId;
+            set => roomId = value;
+        }
+    }
+}
