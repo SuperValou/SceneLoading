db82b9ffe8111da520bf2362465f4fba13a28c47
SuperValou <valentin.esmieu@tuta.io>
2021-07-31 00:10:05 +0200

    WIP: camera switch

---
+++ Assets/Scripts/Controllers/ControllerSwitcher.cs
@@ -0,0 +1,36 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    public class ControllerSwitcher : MonoBehaviour
+    {
+        // -- Inspector
+
+        [Header("Values")]
+        public float switchTime;
+
+        [Header("Parts")]
+        public FirstPersonController firstPersonController;
+        public TopDownController topDownController;
+
+        public CinemachineVirtualCamera firstPersonCamera;
+        public CinemachineVirtualCamera topDownCamera;
+
+        [Header("References")]
+        public AbstractInputManager inputManager;
+
+
+        // -- Class
+
+        void Update()
+        {
+            if (!inputManager.SwitchViewButtonDown())
+            {
+                return;
+            }
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/TopDownController.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Scripts.Utilities;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class TopDownController : MonoBehaviour
+    {
+        // -- Inspector
+
+        [Header("Values")]
+        [Tooltip("How fast the player moves (meters per second).")]
+        public float walkSpeed = 10f;
+
+        [Header("References")]
+        public AbstractInputManager inputManager;
+
+
+        // -- Class
+
+        private CharacterController _controller;
+
+        private Vector3 _velocityVector = Vector3.zero;
+
+        void Start()
+        {
+            _controller = this.GetOrThrow<CharacterController>();
+        }
+
+        void Update()
+        {
+            Vector3 inputMovement = inputManager.GetMoveVector();
+            
+            Vector3 localInputSpeedVector = new Vector3(x: inputMovement.x, y: 0, z: inputMovement.y);
+            Vector3 globalInputSpeedVector = this.transform.TransformDirection(localInputSpeedVector);
+            Vector3 inputSpeedVector = globalInputSpeedVector * walkSpeed;
+
+            _velocityVector.x = inputSpeedVector.x;
+            _velocityVector.z = inputSpeedVector.z;
+            
+            _controller.Move(_velocityVector * Time.deltaTime);
+        }
+    }
+}
